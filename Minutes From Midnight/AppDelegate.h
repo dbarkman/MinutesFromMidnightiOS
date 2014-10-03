32d858cd5dec0f152a3c7d3edde52121253f2ef4
David Barkman <david.barkman13@gmail.com>
2014-10-03 11:55:01 -0700

    Initial Commit

---
+++ Minutes From Midnight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Minutes From Midnight
+//
+//  Created by David Barkman on 10/3/14.
+//  Copyright (c) 2014 David Barkman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Minutes From Midnight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Minutes From Midnight
+//
+//  Created by David Barkman on 10/3/14.
+//  Copyright (c) 2014 David Barkman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
